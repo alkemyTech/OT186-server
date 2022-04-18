ad268220edb8d6347129d8c3c8cc316bb8c0c4e9
“Mariano <emmnlcruz@gmail.com>
2022-04-18 17:34:57 -0300

    Slide Entity and Slide Repository added

---
+++ src/main/java/com/alkemy/ong/repository/SlideRepository.java
@@ -0,0 +1,8 @@
+package com.alkemy.ong.repository;
+
+import com.alkemy.ong.entity.Slide;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SlideRepository extends JpaRepository<Slide, Long> {
+
+}
