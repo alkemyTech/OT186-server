211a3156ab76ceb7eb77ec77ac36f2ffd15373f6
JonnatanMontt <bhaktavisuddha.das@gmail.com>
2022-04-21 15:33:32 -0400

    cambios en Controler

--- src/main/java/com/alkemy/ong/security/WebSecurityConfig.java
+++ src/main/java/com/alkemy/ong/security/WebSecurityConfig.java
@@ -33,8 +33,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .antMatchers(HttpMethod.POST,"/auth/register").permitAll()
-                .antMatchers(HttpMethod.GET,"/auth/login/{id_user}").permitAll()
-                .antMatchers(HttpMethod.GET,"/auth/getAll").permitAll()
                 .antMatchers("/auth/login").permitAll()
                 .anyRequest()
                 .authenticated()
