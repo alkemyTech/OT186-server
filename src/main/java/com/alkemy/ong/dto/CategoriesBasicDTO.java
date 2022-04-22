5dfe1bba127c974ab17e6c594919b98072f3a057
Emanuelamberti <elemanu92@yahoo.com>
2022-04-22 17:42:59 -0300

    Controller actualizado a ticket 41

---
+++ src/main/java/com/alkemy/ong/dto/CategoriesBasicDTO.java
@@ -0,0 +1,13 @@
+package com.alkemy.ong.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+public class CategoriesBasicDTO {
+    private String name;
+
+}
