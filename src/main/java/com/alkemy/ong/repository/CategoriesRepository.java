ad9873cf5077bcb97498c552e884f248876eb69e
Emanuelamberti <elemanu92@yahoo.com>
2022-04-22 17:27:38 -0300

    Arreglado el problema de Repo

--- src/main/java/com/alkemy/ong/repository/CategoriesRepository.java
+++ src/main/java/com/alkemy/ong/repository/CategoriesRepository.java
@@ -1,10 +1,14 @@
 package com.alkemy.ong.repository;
 
-import com.alkemy.ong.entities.Categories;
-import org.springframework.data.repository.CrudRepository;
+import com.alkemy.ong.entity.Categories;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public interface CategoriesRepository extends CrudRepository<Categories, String> {
+import java.util.List;
+import java.util.UUID;
 
+@Repository
+public interface CategoriesRepository extends JpaRepository<Categories, UUID> {
+    List<Categories> findAll(Specification<Categories> spec);
 }
