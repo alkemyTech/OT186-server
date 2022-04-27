0147479d6a9e07cb669c96b062cb432843373b32
AlejandroUnahur <hugoalejandro.vaca@estudiantes.unahur.edu.ar>
2022-04-27 17:15:01 -0300

    OT186 - 54 - Reemplazado anotacion @NotEmpty por @NotBlank para los campos "name" y "email" en ContactsDTO

--- src/main/java/com/alkemy/ong/dto/ContactsDTO.java
+++ src/main/java/com/alkemy/ong/dto/ContactsDTO.java
@@ -3,6 +3,7 @@ package com.alkemy.ong.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import java.util.UUID;
 
@@ -11,12 +12,12 @@ import java.util.UUID;
 public class ContactsDTO {
     private UUID id;
 
-    @NotEmpty(message = "Please provide a name")
+    @NotBlank(message = "Please provide a name")
     private String name;
 
     private Integer phone;
 
-    @NotEmpty(message = "Please provide a email")
+    @NotBlank(message = "Please provide a email")
     private String email;
 
     private String message;
