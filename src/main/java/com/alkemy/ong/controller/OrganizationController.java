d2c90263ed4d99ae43f2ed7e5c27711f92b6cf64
bhaktavisuddha <103943948+bhaktavisuddha@users.noreply.github.com>
2022-04-29 09:31:24 -0400

    Update OrganizationController.java

--- src/main/java/com/alkemy/ong/controller/OrganizationController.java
+++ src/main/java/com/alkemy/ong/controller/OrganizationController.java
@@ -6,16 +6,21 @@ import com.alkemy.ong.repository.OrganizationRepository;
 import com.alkemy.ong.services.OrganizationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.UUID;
 
 @RestController
 @RequestMapping("organization/public")
 public class OrganizationController {
+    
     @Autowired
-    private OrganizationService organizationService;
+    private OrganizationService organizationService;    
     private OrganizationRepository organizationRepository;
+    
     @GetMapping("/{id}")
     public ResponseEntity<OrganizationDTO> getDetailsById(@PathVariable UUID id){
         OrganizationDTO organization = this.organizationService.getDetailsById(id);
