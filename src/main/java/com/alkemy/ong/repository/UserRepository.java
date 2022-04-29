40e388490eed7bb9e31543386314b9bfb1e3a815
bhaktavisuddha <103943948+bhaktavisuddha@users.noreply.github.com>
2022-04-29 09:47:10 -0400

    Update UserRepository.java

--- src/main/java/com/alkemy/ong/repository/UserRepository.java
+++ src/main/java/com/alkemy/ong/repository/UserRepository.java
@@ -1,6 +1,5 @@
 package com.alkemy.ong.repository;
 
-
 import com.alkemy.ong.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
