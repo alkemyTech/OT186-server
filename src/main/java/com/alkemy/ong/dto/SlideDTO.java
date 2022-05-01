a729fb087f3ce00e3156be449e337bbfade132b3
Emanuelamberti <elemanu92@yahoo.com>
2022-05-01 18:13:21 -0300

    Slide controller

---
+++ src/main/java/com/alkemy/ong/dto/SlideDTO.java
@@ -0,0 +1,16 @@
+package com.alkemy.ong.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+public class SlideDTO {
+    private UUID id;
+    private String imageUrl;
+    private String text;
+    private Long order;
+    private Long organizationId;
+}
---
+++ src/main/java/com/alkemy/ong/dto/SlideDTOBasic.java
@@ -0,0 +1,11 @@
+package com.alkemy.ong.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class SlideDTOBasic {
+    private String imageUrl;
+    private Long order;
+}
