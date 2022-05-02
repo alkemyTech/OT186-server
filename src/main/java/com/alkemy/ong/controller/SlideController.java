fad9e1b97d9e2f763d314a119f377bdc4b5c8b71
noebenitez <noebenitez_03@hotmail.com>
2022-05-02 17:49:24 -0300

    Delete slide

--- src/main/java/com/alkemy/ong/controller/SlideController.java
+++ src/main/java/com/alkemy/ong/controller/SlideController.java
@@ -4,12 +4,12 @@ import com.alkemy.ong.dto.SlideDTO;
 import com.alkemy.ong.dto.SlideDTOBasic;
 import com.alkemy.ong.services.SlideService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.List;
 import java.util.UUID;
 
@@ -30,6 +30,17 @@ public class SlideController {
     public ResponseEntity<SlideDTO> getById(@PathVariable UUID id){
         SlideDTO dto = slideService.getDetailsById(id);
         return ResponseEntity.ok().body(dto);
+    }
+
+    @DeleteMapping("/{id}")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public ResponseEntity delete(@PathVariable UUID id){
+        try {
+            slideService.delete(id);
+        }catch (EntityNotFoundException e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 
 }
