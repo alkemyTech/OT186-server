51e31b0479028f2c1bb0dd04bbf5f3869a1333ca
bhaktavisuddha <103943948+bhaktavisuddha@users.noreply.github.com>
2022-05-04 00:21:16 -0400

    Update RoleRepository.java

--- src/main/java/com/alkemy/ong/repository/RoleRepository.java
+++ src/main/java/com/alkemy/ong/repository/RoleRepository.java
@@ -1,7 +1,6 @@
 package com.alkemy.ong.repository;
 
 import com.alkemy.ong.entity.Role;
-import com.alkemy.ong.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
