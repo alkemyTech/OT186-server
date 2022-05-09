98b9be0eb6540dfdaf827dbf972688db69dedfb8
Emanuelamberti <elemanu92@yahoo.com>
2022-05-09 17:12:20 -0300

    Paginacion testimonios

--- src/main/java/com/alkemy/ong/services/TestimonialService.java
+++ src/main/java/com/alkemy/ong/services/TestimonialService.java
@@ -1,6 +1,8 @@
 package com.alkemy.ong.services;
 
+import com.alkemy.ong.dto.PageFormatter;
 import com.alkemy.ong.dto.TestimonialDTO;
+import org.springframework.data.domain.Pageable;
 
 import java.util.UUID;
 
@@ -9,4 +11,5 @@ public interface TestimonialService {
     TestimonialDTO save(TestimonialDTO testimonialDTO);
     TestimonialDTO update(UUID id, TestimonialDTO testimonialDTO);
     void delete(UUID id);
+    public PageFormatter<TestimonialDTO> findPageable(Pageable pageable);
 }
