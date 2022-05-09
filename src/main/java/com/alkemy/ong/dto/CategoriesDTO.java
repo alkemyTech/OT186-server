9e2d293d276feb6a5cc0532cd86158d9990d2e79
noebenitez <noebenitez_03@hotmail.com>
2022-05-09 20:01:07 -0300

    Swagger annotations added in Categories

--- src/main/java/com/alkemy/ong/dto/CategoriesDTO.java
+++ src/main/java/com/alkemy/ong/dto/CategoriesDTO.java
@@ -1,5 +1,7 @@
 package com.alkemy.ong.dto;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,12 +10,20 @@ import java.util.UUID;
 
 @Getter
 @Setter
+@ApiModel
 public class CategoriesDTO {
 
+    @ApiModelProperty(position = 1, value = "123e4567-e89b-12d3-a456-426614174000")
     private UUID id;
+
+    @ApiModelProperty(position = 2, required = true, value = "Campañas")
     @NotBlank(message = "Name must not be blank")
     private String name;
+
+    @ApiModelProperty(position = 3, required = true, value = "Difundir el comienzo o progreso de las campañas.")
     private String description;
+
+    @ApiModelProperty(position = 3, required = true, value = "image.jpg")
     private String image;
 
 }
