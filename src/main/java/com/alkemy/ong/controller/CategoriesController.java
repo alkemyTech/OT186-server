84e0ddec42a6bf6f0effa33f8f11088ffb97353e
noebenitez <noebenitez_03@hotmail.com>
2022-05-18 16:52:49 -0300

    Updated annotations to swagger v3

--- src/main/java/com/alkemy/ong/controller/CategoriesController.java
+++ src/main/java/com/alkemy/ong/controller/CategoriesController.java
@@ -4,7 +4,12 @@ import com.alkemy.ong.dto.CategoriesBasicDTO;
 import com.alkemy.ong.dto.CategoriesDTO;
 import com.alkemy.ong.dto.PageFormatter;
 import com.alkemy.ong.services.CategoriesService;
-import io.swagger.annotations.*;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -19,28 +24,29 @@ import javax.validation.Valid;
 import java.util.List;
 import java.util.UUID;
 
+
 @RestController
 @RequestMapping("/categories")
-@Api(tags = "Categories Controller", description = "Operations on Categories related to News")
+@Tag(description = "Operations on Categories related to News", name = "Categories Controller")
 public class CategoriesController {
 
     @Autowired
     private CategoriesService categoriesService;
 
-    @ApiResponses(value = {
-            @ApiResponse(code = 200, message = "Successful retrieval", response = CategoriesDTO.class),
-            @ApiResponse(code = 404, message = "Category not found")
+    @Operation(summary = "Get Category by Id", responses = {
+            @ApiResponse(responseCode = "200", description = "Successfully operation",content = @Content(schema = @Schema(implementation = CategoriesDTO.class))),
+            @ApiResponse(responseCode = "404", description = "Not Found")
     })
     @GetMapping("/{id}")
     @PreAuthorize("hasRole('ROLE_ADMIN')")
-    public ResponseEntity<CategoriesDTO> getById(   @ApiParam(value = "category_id", required = true)
+    public ResponseEntity<CategoriesDTO> getById(   @Parameter(description = "Id of Category to find", required = true)
                                                     @PathVariable UUID id) {
         CategoriesDTO dto = categoriesService.getDetailsById(id);
         return ResponseEntity.ok().body(dto);
     }
 
-    @ApiResponses(value = {
-            @ApiResponse(code = 200, message = "Successful retrieval", response = CategoriesBasicDTO.class, responseContainer = "PageFormatter"),
+    @Operation(summary = "Get all Categories", responses = {
+            @ApiResponse(responseCode = "200", description = "Successful operation", content = @Content(schema = @Schema(implementation = PageFormatter.class))),
     })
     @GetMapping()
     @PreAuthorize("hasRole('ROLE_ADMIN')")
@@ -49,38 +55,39 @@ public class CategoriesController {
         return ResponseEntity.ok().body(categoriesBasicDTO);
     }
 
-    @ApiResponses(value = {
-            @ApiResponse(code = 201, message = "Created", response = CategoriesDTO.class)
+    @Operation(summary = "Add Category", responses = {
+            @ApiResponse(responseCode = "201", description = "Created", content = @Content(schema = @Schema(implementation = CategoriesDTO.class))),
     })
     @PostMapping
     @PreAuthorize("hasRole('ROLE_ADMIN')")
-    public ResponseEntity<CategoriesDTO> save(  @ApiParam(value = "Category dto to save", required = true)
+    public ResponseEntity<CategoriesDTO> save(  @Parameter(description = "Category dto to save", required = true)
                                                 @Valid @RequestBody CategoriesDTO dto) {
         CategoriesDTO categorySaved = categoriesService.save(dto);
         return ResponseEntity.status(HttpStatus.CREATED).body(categorySaved);
     }
 
-    @ApiResponses(value = {
-            @ApiResponse(code = 200, message = "Successful", response = CategoriesDTO.class),
-            @ApiResponse(code = 404, message = "Category not found")
+    @Operation(summary = "Update Category", responses = {
+            @ApiResponse(responseCode = "200", description = "Successful", content = @Content(schema = @Schema(implementation = CategoriesDTO.class))),
+            @ApiResponse(responseCode = "404", description = "Category not found")
     })
     @PutMapping("/{id}")
     @PreAuthorize("hasRole('ROLE_ADMIN')")
-    public ResponseEntity<CategoriesDTO> update( @ApiParam(value = "category_id", required = true)
+    public ResponseEntity<CategoriesDTO> update( @Parameter(description = "Id of Category to update", required = true)
                                                  @PathVariable UUID id,
-                                                 @ApiParam(value = "Category dto updated", required = true)
+                                                 @Parameter(description = "Category dto updated", required = true)
                                                  @Valid @RequestBody CategoriesDTO dto) {
         CategoriesDTO CategoriesUpdated = categoriesService.update(id, dto);
         return ResponseEntity.ok().body(CategoriesUpdated);
     }
 
-    @ApiResponses(value = {
-            @ApiResponse(code = 204, message = "Category deleted successfully"),
-            @ApiResponse(code = 404, message = "Category not found")
+    @Operation(summary = "Delete Category", responses = {
+            @ApiResponse(responseCode = "204", description = "Category deleted successfully"),
+            @ApiResponse(responseCode = "404", description = "Category not found")
     })
     @DeleteMapping("/{id}")
     @PreAuthorize("hasRole('ROLE_ADMIN')")
-    public ResponseEntity delete(@PathVariable UUID id) {
+    public ResponseEntity delete(   @Parameter(description = "Id of Category to delete", required = true)
+                                    @PathVariable UUID id) {
         try {
             categoriesService.delete(id);
         } catch (EntityNotFoundException e) {
